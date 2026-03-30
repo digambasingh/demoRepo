354811ae0df174185aed1ed9ba87f98fa45be1f1
Digambar singh <digambar.singh_cs22@gla.ac.in>
2026-03-30 11:04:53 +0530

    branch 6 my code

---
+++ mycode/SelectionSort.java
@@ -0,0 +1,17 @@
+package sortingAlgo;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	public static void main(String[] args) {
+		int[] nums = new int[] {12,10,13,11,15,20,16,17,18,1};
+		System.out.println(Arrays.toString(nums));
+		selectionSort(nums);
+		System.out.println(Arrays.toString(nums));
+		
+	}
+	
+	public static void selectionSort(int[] nums) {
+		
+	}
+}
